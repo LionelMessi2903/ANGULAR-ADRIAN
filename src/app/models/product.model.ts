3d813bf64b4889f00c168990f2793cec31ec9c7e
LionelMessi2903 <adribanano2904@gmail.com>
2023-05-09 22:33:26 -0500

    first commit

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,9 @@
+import { Category } from "./category.model";
+
+interface ProductModule {
+    id: number;
+    title: string;
+    price:number;
+    description: string;
+    category:Category;
+}
---
+++ src/app/pages/products/products.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+
+constructor(){}
+
+ngOnInit():void{
+  this.getProducts();
+}
+
+getProducts(){
+
+}
+}
